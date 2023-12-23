db9d51e19bab676f96d98021f6fb6f9187a3e129
Jawad Ali <jawad@eddress.co>
2023-12-23 10:43:05 +0100

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by jawad ali on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
